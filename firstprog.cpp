f6187a84dedbeb0b0f58df9b67d5aa5b81bfb1d8
advika sharmna <sharmaadvikaa@gmail.com>
2024-08-27 12:57:08 +0530

    first prog

--- firstprog.cpp
+++ firstprog.cpp
@@ -1,3 +1,16 @@
-int main(){
-    
+#include <iostream>
+using namespace std;
+
+int main() {
+  //cout << "Hello World!\n";
+  //cout<<2<<endl;
+  //int a = 89;
+  //cout << a++;
+  //cout<<a;
+  //return 0;
+  int a=5;
+  if ( a == 5){
+    cout <<"valid\n";
+  }
+  cout <<"invalid";
 }
