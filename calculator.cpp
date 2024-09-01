8c09592f13837007969e26f8373ac46227cfa2c9
advika sharmna <sharmaadvikaa@gmail.com>
2024-09-01 11:31:24 +0530

    practice codes

---
+++ calculator.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+    int main() {
+    long long A, B;
+    char S;
+    cin >> A >> S >> B;
+    if (S == '+') {
+        cout << A + B << endl;
+    }
+    else if (S == '-') {
+        cout << A - B << endl;
+    }
+    else if (S == '*') {
+        cout << A * B << endl;
+    }
+    else if (S == '/') {
+        cout << A / B << endl;
+    }
+}
