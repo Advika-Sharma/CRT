7c6574065f7a56c18f6b092016e99bcee047847e
advika sharmna <sharmaadvikaa@gmail.com>
2024-09-03 14:02:58 +0530

    practice codes

--- two_intervals.cpp
+++ two_intervals.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 using namespace std;
 int main(){
-    
+    long long n ;
+    cin >> n;
+    bool found_even = false;
+
+    for (int i=2 ; i<=n ; i+=2)
+    {
+        cout << i << endl;
+        found_even=true;
+    } 
+    if (!found_even) {
+        cout << -1 << endl;
+    }
+    return 0;
 }
